f566ec059a4a6d286c1f5a724cd74fade9edb114
wwatanabe331 <wwatanabe331@gmail.com>
2024-10-20 16:15:02 +0900

    webpro課題

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,82 @@
+import { useState } from "react";
+
 export default function App() {
-  const showStory = false;
-  const xItem = "";
-  const yItem = "";
-  const zItem = "";
-  const ukus = "us";
+  const [showStory, setShowStory] = useState(false);
+  const [ukus, setUkUs] = useState("uk");
+  const [name, setName] = useState("Bob");
+  const [bakaStory, setBakaStory] = useState({
+    xItem: "",
+    yItem: "",
+    zItem: "",
+  });
+
+  const xItems = ["Willy the Goblin", "Father Christmas", "Big Daddy"];
+  const yItems = ["Disneyland", "kitchen", "White House"];
+  const zItems = [
+    "turned into a slug and crawled away",
+    " spontaneously combusted",
+  ];
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setName(event.target.customname.value);
+  };
+
+  const handleTaniChange = (event) => {
+    setUkUs(event.target.value);
+  };
+
+  const randomStory = () => {
+    const randomXItem = xItems[Math.floor(Math.random() * xItems.length)];
+    const randomYItem = yItems[Math.floor(Math.random() * yItems.length)];
+    const randomZItem = zItems[Math.floor(Math.random() * zItems.length)];
+
+    setBakaStory({
+      xItem: randomXItem,
+      yItem: randomYItem,
+      zItem: randomZItem,
+    });
+
+    setShowStory(true);
+  };
+
   return (
     <>
       <div>
-        <label htmlFor="customname">Enter custom name:</label>
-        <input type="text" placeholder="" />
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="customname">Enter custom name:</label>
+          <input type="text" id="customname" />
+        </form>
       </div>
+
       <div>
         <label htmlFor="us">US</label>
-        <input type="radio" value="us" checked={ukus === "us"} />
+        <input
+          type="radio"
+          value="us"
+          onChange={handleTaniChange}
+          checked={ukus === "us"}
+        />
         <label htmlFor="uk">UK</label>
-        <input type="radio" value="uk" checked={ukus === "uk"} />
+        <input
+          type="radio"
+          value="uk"
+          onChange={handleTaniChange}
+          checked={ukus === "uk"}
+        />
       </div>
+
       <div>
-        <button>Generate random story</button>
+        <button onClick={randomStory}>Generate random story</button>
       </div>
+
       {showStory && (
         <p>
-          It was 94 fahrenheit outside, so {xItem} went for a walk. When they
-          got to {yItem}, they stared in horror for a few moments, then {zItem}.
-          Bob saw the whole thing, but was not surprised — {xItem} weighs 300
-          pounds, and it was a hot day.
+          It was {ukus === "us" ? "94 fahrenheit" : "34 centigrade"}, so{" "}
+          {bakaStory.xItem} went for a walk. When they got to {bakaStory.yItem},
+          they stared in horror for a few moments, then {bakaStory.zItem}.{name}{" "}
+          saw the whole thing, but was not surprised — {bakaStory.xItem} weighs{" "}
+          {ukus === "us" ? "300 pounds" : "21 stone"}, and it was a hot day.
         </p>
       )}
     </>
